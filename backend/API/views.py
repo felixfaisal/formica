afdb24d54dd962b620fe6d2f7cf81e4c02965495
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-03-24 17:04:29 +0530

    Added basic view to view all the available APIs

--- backend/API/views.py
+++ backend/API/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Base directory of all APIs where all the APIs will be listed")
