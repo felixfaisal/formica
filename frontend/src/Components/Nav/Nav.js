a8e520f0fd0afcf19acf884b886ac3a895eac976
Guna Shekar <gunashekherproddatoori@gmail.com>
2021-03-25 14:24:33 +0530

    add auth state to redux

--- frontend/src/Components/Nav/Nav.js
+++ frontend/src/Components/Nav/Nav.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import Button from "../Button";
 
@@ -7,13 +9,30 @@ import styles from "./Nav.module.css";
 import { ReactComponent as DiscordLogo } from "../../Assets/Images/discord.svg";
 
 const Nav = () => {
+	const { isLoggedIn } = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
+	const history = useHistory();
+
+	const handleLogin = async () => {
+		dispatch({ type: "LOGIN" });
+		history.push("/dashboard");
+	};
+
 	return (
 		<nav className={styles.nav}>
-			<span className={styles.brand}>
+			<Link to="/" className={styles.brand}>
 				<DiscordLogo className={styles.logo} />
 				<h2>Formica</h2>
-			</span>
-			<Button title="Login" />
+			</Link>
+
+			{isLoggedIn ? (
+				<Link to="/dashboard" className={styles.brand}>
+					<h2>Guna Shekar</h2>
+					<DiscordLogo className={styles.logo} />
+				</Link>
+			) : (
+				<Button title="Login" onClick={handleLogin} />
+			)}
 		</nav>
 	);
 };
--- frontend/src/Redux/ConfigureStore.js
+++ frontend/src/Redux/ConfigureStore.js
@@ -1,11 +1,10 @@
 import { createStore, combineReducers } from "redux";
 
 import { example } from "./Reducers/example.reducer";
+import { auth } from "./Reducers/auth.reducer";
 
 export const ConfigureStore = () => {
-  const store = createStore(
-    combineReducers({ example }),
-  );
+	const store = createStore(combineReducers({ example, auth }));
 
-  return store;
+	return store;
 };
