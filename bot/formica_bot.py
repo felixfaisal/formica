fc98384a4d23e63a89928818ec4cc023a19e9842
n-maido <nhi_maido@outlook.com>
2021-03-24 23:09:21 -0700

    detect reactions to welcome msg and DM user; fix token issue

--- bot/formica_bot.py
+++ bot/formica_bot.py
@@ -1,14 +1,19 @@
 import discord
 import os
+from decouple import config
 
 
 client = discord.Client()
+
+# define messages
+welcome_msg = "Welcome to Formica, the in-discord form service! It looks like you have a form to fill out. To do so, please react to this message with any emoji. Then, check your inbox!"
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-# listen for messages
+# listen for messages.commands
 @client.event
 async def on_message(message):
     msg = message.content
@@ -19,9 +24,16 @@ async def on_message(message):
 
     # listen for commands
     if msg.startswith('!formica'):
-        welcome = "Welcome to Formica, the in-discord form service! It looks like you have a form to fill out. To do so, please react to this message with any emoji. Then, check your inbox!"
         #display welcome message
-        await message.channel.send(welcome)
+        await message.channel.send(welcome_msg)
+
+# listen for reactions
+@client.event
+async def on_reaction_add(reaction, user):
+    if reaction.message.content == welcome_msg:
+        await user.send("hi")
 
 # run bot
-client.run(os.getenv('TOKEN'))
+BOT_TOKEN = config('TOKEN')
+client.run(BOT_TOKEN)
+
