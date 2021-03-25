28c512dc3134e89125f0975b880cb5674b37a4a6
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-03-25 17:11:24 +0530

    Sucessfully fetch user data after authorization, use of env file

--- backend/API/views.py
+++ backend/API/views.py
@@ -5,11 +5,17 @@ from .serializer import TaskSerializer
 from .models import Task
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+import requests
+import environ 
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 redirect_url_discord = "https://discord.com/api/oauth2/authorize?client_id=728306573696303135&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Foauth2%2Flogin%2Fredirect%2F&response_type=code&scope=identify"
 @api_view(["GET"])
 def index(request):
+    print(os.getenv("CLIENT_ID"))
     api_urls = {
         "List":"/list/",
         "Detail View":"/list-detail/<str:pk>/",
@@ -24,7 +30,32 @@ def discord_login(request):
     return redirect(redirect_url_discord)
 
 def discord_login_redirect(request):
-    return JsonResponse({"msg":"I'm authenticated"})
+    code = request.GET.get('code')
+    print(code)
+    credentials = exchange_code(code)
+    return JsonResponse(credentials)
+
+def exchange_code(code):
+    data = {
+        "client_id":os.getenv("CLIENT_ID"),
+        "client_secret":os.getenv("CLIENT_SECRET"),
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": "http://localhost:8000/oauth2/login/redirect/", 
+        "scope": "identify"
+    }
+    headers = {
+        "Content-type": "application/x-www-form-urlencoded"
+    }
+    response = requests.post("https://discord.com/api/oauth2/token", data=data, headers=headers)
+    #print(response.json())
+    credentials = response.json()
+    access_token = credentials['access_token']
+    response = requests.get("https://discord.com/api/v6/users/@me", headers={
+        'Authorization': 'Bearer %s' % access_token
+    })
+    user = response.json()
+    return user
 
 @api_view(["GET"])
 def tasklist(request):
