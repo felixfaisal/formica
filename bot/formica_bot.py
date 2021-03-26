f4727d3e895e21bdb78a88feddaa76334d899d71
n-maido <nhi_maido@outlook.com>
2021-03-25 21:51:02 -0700

    start form: display 1 question, then detect and save 1 response

--- bot/formica_bot.py
+++ bot/formica_bot.py
@@ -5,10 +5,23 @@ from decouple import config
 
 client = discord.Client()
 
+form_color = 0xff8906
 # define messages
 welcome_title = "Welcome to Formica, the in-discord form service!"
 welcome_msg = "It looks like you have a form to fill out. To do so, please react to this message with any emoji. Then, check your inbox!"
 form_name = "Event Registration"
+
+
+form_init_msg = "To start, type !start"
+
+# tmp
+cur_q = "What is your first name?"
+cur_q_description = " "
+cur_response = "response"
+
+def start_form():
+    cur_q_embed = discord.Embed(title = cur_q, description = cur_q_description, color = form_color)
+    return cur_q_embed
 
 @client.event
 async def on_ready():
@@ -25,12 +38,34 @@ async def on_message(message):
 
     # listen for commands
     if msg.startswith('!formica'):
-        #display welcome message
         #embed constructor
-        welcome_embed = discord.Embed(title = welcome_title, description = welcome_msg, color = 0xff8906)
+        welcome_embed = discord.Embed(title = welcome_title, description = welcome_msg, color = form_color)
         welcome_embed.add_field(name = "Form: ", value = form_name, inline = False)
 
+        # send welcome message
         await message.channel.send(embed=welcome_embed)
+    
+    if msg.startswith('!start'):
+        # send form start prompt
+        start_embed = start_form()
+        await message.channel.send(embed=start_embed)
+
+         # wait for response
+        def check(m):
+            # check that it's the right user and channel
+            # later: check that we're on the right question or else the form restarts
+            return (m.author.id == message.author.id and m.channel == message.channel):
+
+        msg = await client.wait_for('message', check=check)
+
+        # save response
+        cur_response = msg.content
+
+        # send feedback to user
+        await message.channel.send(f"Response received: {cur_response}")
+
+
+
 
 # listen for reactions
 @client.event
@@ -40,7 +75,12 @@ async def on_reaction_add(reaction, user):
 
     # check if the message was the welcome msg
     if message_title == welcome_title:
-        await user.send("hi")
+        # send the form instruction message to the user
+        form_init = discord.Embed(title = form_name, description = form_init_msg, color = form_color)
+        form_init.add_field(name = "Instructions: ", value = "Respond to my questions by typing a message like you normally would!\n You can edit your response by hovering on your message and clicking 'edit'.\n To see a list of available commands, type !help.", inline = False)
+
+        await user.send(embed=form_init)
+
 
 
 # run bot
