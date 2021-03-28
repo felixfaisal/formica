18d7762b4be9df62b022e1494f278f3bac0cd29d
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-03-29 04:29:26 +0530

    Added Discord User model

--- backend/API/models.py
+++ backend/API/models.py
@@ -7,3 +7,13 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+class DiscordUser(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    discord_tag = models.CharField(max_length=100)
+    avatar = models.CharField(max_length=100)
+    public_flags = models.IntegerField()
+    flags = models.IntegerField()
+    locale = models.CharField(max_length=100)
+    mfa_enabled = models.BooleanField()
+    last_login = models.DateTimeField()
