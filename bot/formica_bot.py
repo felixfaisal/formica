669d485073fb7a32028d9bf8f428f35c11dd9d9d
n-maido <nhi_maido@outlook.com>
2021-03-28 19:54:28 -0700

    send an answer confirmation message

--- bot/formica_bot.py
+++ bot/formica_bot.py
@@ -16,10 +16,8 @@ form_name = "Event Registration"
 
 form_init_msg = "To start, type !start"
 
-# tmp
-cur_q = "What is your first name?"
-cur_q_description = " "
-cur_response = "response"
+
+
 form_started = False
 
 def start_form():
@@ -27,16 +25,21 @@ def start_form():
     with open("dummy_questions.json", "r") as q:
         questions = json.load(q)
         q_count = len(questions)
-    return questions, q_count
+    
+    with open('dummy_responses.json', 'r') as r:
+        responses = json.load(r)
+
+    return questions, q_count, responses
+
 
 def get_question(questions, cur_index):
     cur_q_embed = discord.Embed(title = questions[cur_index]['question'], description = questions[cur_index]['description'], color = form_color)
     return cur_q_embed
 
-def set_response(response, author, index):
+def set_response(responses, response, author, index):
     # get the responses from the database
-    with open('dummy_responses.json', 'r') as r:
-        database_responses = json.load(r)
+    # with open('dummy_responses.json', 'r') as r:
+    #     responses = json.load(r)
     
     # test
     # print("retrieved responses: ", database_responses)
@@ -45,26 +48,43 @@ def set_response(response, author, index):
 
     # search database for the user
     try:
-        target = next(user for user in database_responses if user['username'] == str(author) )
+        target = next(user for user in responses if user['username'] == str(author) )
         # print("target: ", target)
 
         # get index
-        target_index = database_responses.index(target)
+        target_index = responses.index(target)
         print("found at index ", target_index)    
 
         #set response
-        database_responses[target_index]['responses'].append(response)
-        print("set: ", database_responses)
+        responses[target_index]['responses'].append(response)
+        print("set: ", responses)
     except:
         print("not found")
         # append to database
-        database_responses.append({'username': str(author), 'user_id': str(author.id), 'responses': [response]})
-        print("appended: ", database_responses)
+        responses.append({'username': str(author), 'user_id': str(author.id), 'responses': [response]})
+        print("appended: ", responses)
+
+        target_index = len(responses) - 1
+    
+    return target_index
 
     
     #write to the database
-    with open('dummy_responses.json', 'w') as w:
-        json.dump(database_responses, w)
+    # with open('dummy_responses.json', 'w') as w:
+    #     json.dump(responses, w)
+
+def end_form(questions, responses, author_index):
+    form_started = False
+    confirmation_embed = discord.Embed(title = 'Confirm your answers', description = 'Click ✅ to submit.\n If you need to edit your answers, go back and do so, then type !finished.', color = form_color)
+
+    print("responses: ", responses)
+    print("1st q: ", questions[0]['question'])
+    print("1st response: ", responses[author_index]['responses'][0])
+
+    for item in range(len(questions)):
+        confirmation_embed.add_field(name = questions[item]['question'], value = responses[author_index]['responses'][item], inline = False)
+    
+    return confirmation_embed
 
 
 @client.event
@@ -93,7 +113,8 @@ async def on_message(message):
     if msg.startswith('!start'):
         if form_started == False:
             cur_index = 0
-            questions, q_count = start_form()
+            questions, q_count, responses = start_form()
+            
         
 
         while cur_index < q_count:
@@ -113,7 +134,7 @@ async def on_message(message):
 
             # save response
             cur_response = msg.content
-            set_response(msg.content, message.author, cur_index)
+            author_index = set_response(responses, msg.content, message.author, cur_index)
 
             # send feedback to user
             # await message.channel.send(f"Response received: {msg.content}")
@@ -121,7 +142,9 @@ async def on_message(message):
             # update counters
             cur_index += 1
         
-        await message.channel.send("Questions completed")
+        #await message.channel.send("Questions completed")
+        confirmation_embed = end_form(questions, responses, author_index)
+        await message.channel.send(embed=confirmation_embed)
         
 
 
