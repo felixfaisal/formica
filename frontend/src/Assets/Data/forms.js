db2e1255304d359c6f27902585fa6f20f13f671b
Guna Shekar <gunashekherproddatoori@gmail.com>
2021-03-31 15:45:05 +0530

    implement form data page

--- frontend/src/Assets/Data/forms.js
+++ frontend/src/Assets/Data/forms.js
@@ -1,23 +1,27 @@
 export default [
 	{
+		id: 1,
 		title: "Among Us Feedback",
 		responses: 125,
 		shared: 6,
 		accepting: true,
 	},
 	{
+		id: 2,
 		title: "Suitable Timing for Twitch Streaming in Latin America",
 		responses: 299,
 		shared: 8,
 		accepting: true,
 	},
 	{
+		id: 3,
 		title: "Among Us Feedback",
 		responses: 125,
 		shared: 6,
 		accepting: false,
 	},
 	{
+		id: 4,
 		title: "Among Us Feedback",
 		responses: 125,
 		shared: 6,
