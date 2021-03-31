773b8418de0a17ce6b68e800669addc43057921a
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-03-31 20:31:11 +0530

    Migrated to MySQL

--- backend/formica/settings.py
+++ backend/formica/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'formica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'formica',
+        'USER': 'newuser',
+        'PASSWORD': 'user_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
