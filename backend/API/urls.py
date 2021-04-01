075261f4c5340c3b241fc4cad83d24f2b51923e0
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-04-01 19:55:45 +0530

    Fixed API routes

--- backend/API/urls.py
+++ backend/API/urls.py
@@ -4,15 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('list/', views.tasklist, name='list'),
-    path('logout/', views.discord_logout, name='logout'),
-    path('listcreate/', views.listcreate, name='listcreate'), 
-    path('listdetail/<str:pk>/', views.taskdetail, name='listdetail'),
-    path('listupdate/<str:pk>/', views.listupdate, name='listupdate'),
-    path('listdelete/<str:pk>/', views.listdelete, name='listdelete'),
-    path('login/', views.discord_login, name='login'),
-    path('login/redirect/', views.discord_login_redirect, name='loginredirect'),
-    path('forms/', views.formlist, name='formlist'), 
-    path('responses/', views.responselist, name='responselist'), 
-    path('formcreate/', views.formcreateresponse, name='formcreateresponse')
+    path('oauth2/logout/', views.discord_logout, name='logout'),
+    path('api/listcreate/', views.listcreate, name='listcreate'), 
+    path('oauth2/login/', views.discord_login, name='login'),
+    path('oauth2/login/redirect/', views.discord_login_redirect, name='loginredirect'),
+    path('api/forms/', views.formlist, name='formlist'), 
+    path('api/responses/', views.responselist, name='responselist'), 
+    path('api/formcreate/', views.formcreateresponse, name='formcreateresponse')
 ]
--- backend/formica/urls.py
+++ backend/formica/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('oauth2/', include('API.urls')),
+    path('', include('API.urls')),
 ]
