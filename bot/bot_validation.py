4339a972f9fe6f3877432ae80de5dc80cce33062
n-maido <nhi_maido@outlook.com>
2021-04-04 20:16:08 -0700

    tweak phone regex to account for all country codes

--- bot/bot_validation.py
+++ bot/bot_validation.py
@@ -3,7 +3,7 @@ import re
 # define regex patterns
 email_regex = r"^\w+((-|\.|_)\w+)*\w*@\w+(-\w)?(\w*)?\.\w{2,3}(\.\w{2,3})?$"
 
-phone_regex = r"^\+\d{1,3}\s(\d{3}\s){2}\d{4}$" # follows the ITU format for international phone numbers (+X XXX XXX XXX)
+phone_regex = r"^\+(\d{1,3}|(\d{1,2}\-\d{3,4}))\s(\d{3}\s){2}\d{4}$" # follows the ITU format for international phone numbers (+X XXX XXX XXX)
 
 number_regex = r"\d+"
 
