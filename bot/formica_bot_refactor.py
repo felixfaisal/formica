05685d3c193b73ed0687a43bd69ab07315f87648
n-maido <nhi_maido@outlook.com>
2021-04-08 23:57:01 -0700

    cleaning up

--- bot/formica_bot_refactor.py
+++ bot/formica_bot_refactor.py
@@ -4,8 +4,8 @@ import discord
 from discord.ext import commands
 import os
 import json
-#from decouple import config
-from dotenv import load_dotenv
+from decouple import config
+#from dotenv import load_dotenv
 
 import globals 
 
@@ -21,7 +21,7 @@ from bot_functions import end_form
 
 from bot_validation import validate_response
 
-load_dotenv()
+#load_dotenv()
 
 intents = discord.Intents().all()
 intents.reactions = True
@@ -81,6 +81,7 @@ async def formica(ctx, *, received_name: str): # so we don't have to wrap form n
             print(target)
 
             # get the index of the form
+            
             globals.form_index = globals.forms.index(target)
             print("form index is: ", globals.form_index)
 
@@ -278,7 +279,7 @@ async def on_reaction_add(reaction, user):
             await old_confirmation.edit(embed = new_confirmation)
 
 # run bot
-BOT_TOKEN = os.getenv("TOKEN")
-#BOT_TOKEN = config("TOKEN")
+#BOT_TOKEN = os.getenv("TOKEN")
+BOT_TOKEN = config("TOKEN")
 client.run(BOT_TOKEN)
 
--- bot/globals.py
+++ bot/globals.py
@@ -1,24 +1,17 @@
 # form details
-form_started = False
 form_name = "Name"
 form_alert_channel = ""
 form_color = 0xff8906 #colour of the embed msgs
 form_index = 0
 forms = []
 welcome_ids = []
-
-# question details
-trackers = {}
-local_responses = []
-questions = []
 mc_ids = []
 tot_options = 0 #tot multiple choice options
 
-# user details (the person filling out the form)
-user_index = 0
+# keep track of users, questions, and responses
+trackers = {}
+local_responses = []
+questions = []
 
 # multiple choice emojis
 emoji_options = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣']
-
-# global messages
-confirmation_id = 0
